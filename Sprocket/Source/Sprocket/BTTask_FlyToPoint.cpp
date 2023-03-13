74ce0c73c17eb7c10381d40429b80ef3175fd284
Caitlynn Platt <jamie.liddiard.platt@live.co.uk>
2023-03-13 09:26:36 +0000

    UniMorningWork

---
+++ Sprocket/Source/Sprocket/BTTask_FlyToPoint.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FlyToPoint.h"
+
+EBTNodeResult::Type UBTTask_FlyToPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    return EBTNodeResult::Type();
+}
---
+++ Sprocket/Source/Sprocket/BTTask_FlyToPoint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_FlyToPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPROCKET_API UBTTask_FlyToPoint : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+};
